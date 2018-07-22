5b79f269863be620e7bb83f1adfe54866350bd75
Carrah Osborn <carraheosborn@hotmail.com>
2018-07-22 15:14:22 -0500

    adds object notes and passing test 2.

--- test/2-objects.spec.js
+++ test/2-objects.spec.js
@@ -1,16 +1,26 @@
 const { expect } = require('chai');
 
 // 1. Create an object called "company" with a property called "name" to hold the value "XYZ Corporation"
-
+var company = { name: 'XYZ Corporation' };
 // 2. Add a property called "employeeCount" to hold the value 3
-
+company.employeeCount = 3;
 // 3. Add a property called "employeeNames" to hold the names of the
 // company's employees: "Susan", "Becky", "Carl"
-
-// 4. Add a property called "companyGreeting" to contain the sentence:
+company.employeeNames = ['Susan', 'Becky', 'Carl'];
+// 4. Add a property called "greeting" to contain the sentence:
 // "XYZ corporation has 3 employees. Their names are Susan, Becky, and Carl"
+company.greeting =
+  company.name +
+  ' has ' +
+  company.employeeCount +
+  ' employees. Their names are ' +
+  company.employeeNames[0] +
+  ', ' +
+  company.employeeNames[1] +
+  ', and ' +
+  company.employeeNames[2];
 
-describe('1. objects', function() {
+describe('2. objects', function() {
   it('Should 100% pass', function() {
     const { name, employeeCount, employeeNames, greeting } = company;
     const expected =
