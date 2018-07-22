84aad2bdfae0a21658349cb5d08818b4d33d6b42
Carrah Osborn <carraheosborn@hotmail.com>
2018-07-21 22:43:16 -0500

    adds completed test 1

--- test/1-data-types-and-variables.spec.js
+++ test/1-data-types-and-variables.spec.js
@@ -1,18 +1,27 @@
-const { test1 } = require('../util/test-util');
+const { test1 } = require("../util/test-util");
 
 // 1. Create a variable called "company" to hold the name of a company called "XYZ Corporation"
-
+var company = "XYZ Corporation";
 // 2. Create a variable called "employeeCount" to hold the value 3
-
+var employeeCount = 3;
 // 3. Create a variable called "employeeNames" to hold the names of the
 // company's employees: "Susan", "Becky", "Carl"
-
+var employeeNames = ["Susan", "Becky", "Carl"];
 // 4. Create a variable called "companyGreeting" to contain the sentence:
 // "XYZ corporation has 3 employees. Their names are Susan, Becky, and Carl"
-
-describe('1. data types and variables', function() {
+var companyGreeting =
+  company +
+  " has " +
+  employeeCount +
+  " employees. Their names are " +
+  employeeNames[0] +
+  ", " +
+  employeeNames[1] +
+  ", and " +
+  employeeNames[2];
+describe("1. data types and variables", function() {
   it(
-    'Should 100% pass',
+    "Should 100% pass",
     test1(companyGreeting, company, employeeCount, employeeNames)
   );
 });
