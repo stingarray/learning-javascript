5389e3e3f311b4a8d8fbd89de687354d53b1f0a4
Carrah Osborn <carraheosborn@hotmail.com>
2018-08-01 18:50:54 -0500

    Adds notes for variables

---
+++ notes/variables-2.js
@@ -0,0 +1,28 @@
+var isSkyBlue = true;
+
+var skyColor = 'blue';
+
+var planetCount = 8;
+
+var planets = [
+  'Mercury',
+  'Venus',
+  'Earth',
+  'Mars',
+  'Jupiter',
+  'Saturn',
+  'Uranus',
+  'Neptune'
+];
+
+console.log(planets[7]);
+
+var kitties = ['Chinook', 'Walker', 'Texas', 'Ranger'];
+
+var mommaKitty = kitties[0];
+
+console.log(mommaKitty);
+
+var calicoBabyKitty = kitties[2];
+
+console.log(calicoBabyKitty);
---
+++ notes/variables.js
@@ -0,0 +1,26 @@
+var firstName = 'Susan';
+
+console.log(firstName);
+
+var age = 47;
+
+console.log(age);
+
+var hasPets = true;
+
+// If hasPets is true, write "Susan has pets".
+// Otherwise write "Susan does not have pets".
+
+if (hasPets) {
+  console.log('Susan has pets');
+} else {
+  console.log('Susan does not have pets');
+}
+
+var person = {
+  firstName: firstName,
+  age: age,
+  hasPets: hasPets
+};
+
+console.log(person);
